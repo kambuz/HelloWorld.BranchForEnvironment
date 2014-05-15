caef295720f27bff8102a1483c044d7380b502a0
kambuz <kambuz@users.noreply.github.com>
2014-05-15 15:42:18 +0300

    For QA environment.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
-    System.out.println("This is for development environment.");
+    System.out.println("This is for test environment.");
   }
 }
